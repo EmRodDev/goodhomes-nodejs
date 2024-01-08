f3952954b469a57e06e356839d32be49a88b1ac9
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-08 06:00:27 -0300

    Defined relations for 'Message' model

--- models/relations.js
+++ models/relations.js
@@ -2,14 +2,20 @@ import Property from './Property.js';
 import Price from './Price.js';
 import Category from './Category.js';
 import User from './User.js';
+import Message from './Message.js';
 
 Property.belongsTo(Price, {foreignKey: 'priceId', as: 'price'});
 Property.belongsTo(Category, {foreignKey: 'categoryId', as: 'category'});
 Property.belongsTo(User, {foreignKey: 'userId', as: 'user'});
 
+Message.belongsTo(Property, {foreignKey: 'propertyId'});
+Message.belongsTo(User, {foreignKey: 'userId'});
+
+
 export {
     Property,
     Price,
     Category,
-    User
+    User,
+    Message
 }
