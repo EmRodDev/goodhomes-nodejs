36c6a713e642e406cb9d67f24205ad1114dcfed6
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-08 05:54:43 -0300

    'apartments' mispelling correction, category() function logic added, notFound() function logic added, searcher() function logic added

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,8 +1,9 @@
+import {Sequelize} from 'sequelize';
 import {Price, Category, Property} from '../models/relations.js';
 
 const home = async (req, res) => {
 
-    const [categories, prices, houses, departments] = await Promise.all([
+    const [categories, prices, houses, apartments] = await Promise.all([
         Category.findAll({raw: true}),
         Price.findAll({raw: true}),
         Property.findAll({
@@ -40,20 +41,75 @@ const home = async (req, res) => {
         categories,
         prices,
         houses,
-        departments
+        apartments,
+        csrfToken: req.csrfToken()
     });
 }
 
-const category = (req, res) => {
+const category = async (req, res) => {
+    const {id} = req.params;
 
+    //Verify that the category exists
+    const category = await Category.findByPk(id);
+
+    if(!category){
+        return res.redirect('/404');
+    }
+
+    //Get the properties of the category
+    const properties = await Property.findAll({
+        where: {
+            categoryId: id
+        },
+        include : [
+            {model: Price, as: 'price'}
+        ]
+    });
+
+    res.render('category', {
+        page: `${category.name}s on Sale`,
+        properties,
+        csrfToken: req.csrfToken()
+    })
 }
 
 const notFound = (req, res) => {
+    res.render('404', {
+        page: 'Not Found',
+        csrfToken: req.csrfToken()
+    })
 
 }
 
-const searcher = (req, res) => {
+const searcher = async (req, res) => {
+    const {term} = req.body;
 
+    //Validate that the term is not empty
+
+    if(!term.trim()){
+        return res.redirect('back');
+    }
+
+    //Get the properties
+    const properties = await Property.findAll({
+        where: {
+            title: {
+                [Sequelize.Op.like] : '%' + term + '%'
+            }
+        },
+        include: [
+            {
+            model: Price, as: 'price'
+            }
+        ],
+        order: [['createdAt','DESC']]
+    });
+
+    res.render('searcher',{
+        page: 'Results from the search',
+        properties,
+        csrfToken: req.csrfToken()
+    })
 }
 
 
