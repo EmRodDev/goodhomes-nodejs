d9d5ac892393a121afe623291bfb247f01ebe1c4
EmRodr <emanuelrodriguez203@gmail.com>
2024-01-14 18:21:14 -0300

    Corrected a mispelling on environment variables, increased the pool.max variable to 10

--- config/db.js
+++ config/db.js
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: '.env'});
 
-const db = new Sequelize(process.env.BD_NAME,process.env.BD_USER,process.env.BD_PASS ?? '',{
-    host: process.env.BD_HOST,
-    port: 3307,
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS ?? '',{
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define:{
         timestamps: true
     },
     pool:{
-        max: 5,
+        max: 10,
         min: 0,
         acquire: 30000,
         idle: 10000
